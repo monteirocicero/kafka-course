c5f1796a1bd44826315db14090feaadf0e859240
Cicero Monteiro <cicerolmonteiro@gmail.com>
2019-03-19 22:06:57 -0300

    first commit

---
+++ src/main/java/com/cicero/kafka/ProducerDemoWithCallback.java
@@ -0,0 +1,48 @@
+
+package com.cicero.kafka;
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class ProducerDemoWithCallback {
+
+    public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(ProducerDemoWithCallback.class);
+        String bootsrapServers = "127.0.0.1:9092";
+
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootsrapServers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+        for (int i = 0; i < 10; i++) {
+            ProducerRecord<String, String> record = new ProducerRecord<String, String>("first_topic", "Hellow world!" + i);
+
+            producer.send(record, new Callback() {
+                @Override
+                public void onCompletion(RecordMetadata metadata, Exception exception) {
+                    if (exception == null) {
+                        logger.info("Received new metadata.\n" +
+                                "Topic: " + metadata.topic() + "\n" +
+                                "Particion: " + metadata.partition() + "\n" +
+                                "Offset: " + metadata.offset() + "\n" +
+                                "Timestamp: " + metadata.timestamp());
+                    } else {
+                        logger.error("Error while producing", exception);
+                    }
+                }
+            });
+        }
+
+
+                producer.flush();
+
+                producer.close();
+    }
+}
---
+++ src/main/java/com/cicero/kafka/ProducerDemoWithKey.java
@@ -0,0 +1,57 @@
+
+package com.cicero.kafka;
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+public class ProducerDemoWithKey {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        Logger logger = LoggerFactory.getLogger(ProducerDemoWithKey.class);
+        String bootsrapServers = "127.0.0.1:9092";
+
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootsrapServers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+
+        for (int i = 0; i < 10; i++) {
+            String topic = "first_topic";
+            String value = "Hello world " + Integer.toString(i);
+            String key = "_id" + Integer.toString(i);
+
+            logger.info("Key: " + key);
+
+            ProducerRecord<String, String> record = new ProducerRecord<String, String>(topic, key, value);
+
+
+            producer.send(record, new Callback() {
+                        @Override
+                        public void onCompletion(RecordMetadata metadata, Exception exception) {
+                            if (exception == null) {
+                                logger.info("Received new metadata.\n" +
+                                        "Topic: " + metadata.topic() + "\n" +
+                                        "Particion: " + metadata.partition() + "\n" +
+                                        "Offset: " + metadata.offset() + "\n" +
+                                        "Timestamp: " + metadata.timestamp());
+                            } else {
+                                logger.error("Error while producing", exception);
+                            }
+                        }
+                    }).get();
+        }
+
+
+                producer.flush();
+
+                producer.close();
+    }
+}

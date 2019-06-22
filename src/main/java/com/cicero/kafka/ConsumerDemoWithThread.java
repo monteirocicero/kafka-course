31a2c1e6dc1f810aec1faa6fa228ae92e197b17d
Cicero Monteiro <cicerolmonteiro@gmail.com>
2019-06-22 18:43:20 -0300

    Adding the consumer with thread

---
+++ src/main/java/com/cicero/kafka/ConsumerDemoWithThread.java
@@ -0,0 +1,113 @@
+
+package com.cicero.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.errors.WakeupException;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
+
+public class ConsumerDemoWithThread {
+
+    public static void main(String[] args) {
+        new ConsumerDemoWithThread().run();
+    }
+
+    private ConsumerDemoWithThread() {
+
+    }
+
+    private void run() {
+        Logger logger = LoggerFactory.getLogger(ConsumerDemoWithThread.class);
+
+        String bootsrapServers = "127.0.0.1:9092";
+        String topic = "first_topic";
+        String groupId = "my-sixth-application";
+
+        logger.info("Creating the consumer thread");
+        CountDownLatch latch = new CountDownLatch(1);
+        Runnable myConsumerRunnable = new ConsumerRunnable(latch,bootsrapServers,groupId,topic);
+
+        Thread myThread = new Thread(myConsumerRunnable);
+        myThread.start();
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            logger.info("Caught shutdonw hook");
+            ((ConsumerRunnable) myConsumerRunnable).shutddown();
+            try {
+                latch.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            logger.info("Application has exited");
+        }));
+
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            logger.error("Application got interrupted", e);
+        } finally {
+            logger.info("Application is closing");
+        }
+    }
+
+
+
+    public class ConsumerRunnable implements Runnable {
+
+        Logger logger = LoggerFactory.getLogger(ConsumerRunnable.class);
+
+        private final CountDownLatch latch;
+
+        KafkaConsumer<String, String> consumer;
+
+
+        public ConsumerRunnable(CountDownLatch latch, String bootsrapServers, String groupId, String topic) {
+            this.latch = latch;
+            Properties properties = new Properties();
+            properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootsrapServers);
+            properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+            properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+            properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+            properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+            consumer = new KafkaConsumer<String, String>(properties);
+            consumer.subscribe(Arrays.asList(topic));
+
+        }
+
+        @Override
+        public void run() {
+            try {
+                while(true) {
+                    ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+
+                    for (ConsumerRecord<String, String> record : records) {
+                        logger.info("Key: " + record.key() + ", Value: " + record.value());
+                        logger.info("Particion: " + record.partition() + ", Offset: " + record.offset());
+                    }
+                }
+
+            } catch (WakeupException e) {
+                logger.info("Received shutdown signal");
+
+            } finally {
+                consumer.close();
+                latch.countDown();
+            }
+
+        }
+
+        public void shutddown() {
+            consumer.wakeup();
+
+        }
+    }
+}
